18c7d6cd666942d3a70859a8533fa30d3172d7e0
Luiz <luiz.hse@hotmail.com>
2017-08-07 21:31:25 -0300

    Add Arquivos de Configuracao

---
+++ 01-quick-start/aula03-types/types.js
@@ -0,0 +1,11 @@
+function log(message) {
+    console.log(message);
+}
+log('Hello world Typescript!');
+/**
+ * string
+ * number
+ * boolean
+ * arrays
+ */ 
+//# sourceMappingURL=types.js.map
---
+++ 01-quick-start/aula03-types/types.ts
@@ -0,0 +1,11 @@
+function log(message: string){ //any no lugar de string aceitaria qualquer tipo de variável
+    console.log(message);
+}
+
+log('Hello world Typescript!');
+/**
+ * string
+ * number
+ * boolean 
+ * arrays
+ */
