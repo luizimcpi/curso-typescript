9262a3391d0e4b51ca1c3d66615dcd9d1f2084c2
Luiz <luiz.evangelista@concrete.com.br>
2017-09-18 10:00:26 -0300

    generics

---
+++ 01-quick-start/aula09-generics/run.ts
@@ -0,0 +1,7 @@
+import { Dao } from './dao';
+import { Animal } from './../aula07-classes/animal';
+
+let dao: Dao<Animal> = new Dao<Animal>();
+let animal: Animal = new Animal('Rex');
+
+dao.insert(animal);
