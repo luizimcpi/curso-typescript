8ea1ec9d73505e6bfa23a0a690787610b795e4de
Luiz <luiz.evangelista@concrete.com.br>
2017-11-07 09:11:20 -0200

    Criando Modulo de Contato

--- angular2/01-cadastro-contatos/app/app.module.ts
+++ angular2/01-cadastro-contatos/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ContatosModule } from './contatos/contatos.module';
 
 //Decorators do Angular 2 as classes ts sem decorators são apenas typescript puro
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        ContatosModule
+    ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
