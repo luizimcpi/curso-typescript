2bf86be38b52d2a15c82808b4e4c80923139b196
Luiz_Henrique <luizhenrique.se@gmail.com>
2017-11-10 16:17:51 -0200

    Adiciona Routing

--- angular2/01-cadastro-contatos/app/app.module.ts
+++ angular2/01-cadastro-contatos/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ContatosModule } from './contatos/contatos.module';
 
 //Decorators do Angular 2 as classes ts sem decorators são apenas typescript puro
 @NgModule({
     imports: [
+        AppRoutingModule,
         BrowserModule,
         ContatosModule
     ],
