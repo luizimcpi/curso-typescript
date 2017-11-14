47620c4950e53bfad0e59e5db04534c1080ba839
Luiz <luiz.evangelista@concrete.com.br>
2017-11-14 16:04:04 -0200

    Adiciona get assincrono com Promises e utiliza ArrowFunctions

--- angular2/01-cadastro-contatos/app/contatos/contatos-lista.component.ts
+++ angular2/01-cadastro-contatos/app/contatos/contatos-lista.component.ts
@@ -14,6 +14,10 @@ export class ContatosListaComponent implements OnInit {
     contatos: Contato[];
     constructor(private contatoService: ContatoService) { }
     ngOnInit(): void {
-        this.contatos = this.contatoService.getContatos();
+        this.contatoService.getContatos()
+        //Arrow Function
+        .then((contatos: Contato[]) => {
+            this.contatos = contatos;
+        }).catch(err => console.log(err));
     }
 }
