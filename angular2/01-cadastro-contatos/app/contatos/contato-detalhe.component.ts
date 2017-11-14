4fc5c2c79bb3db29c2763f0f57ab80d5a46526b9
Luiz <luiz.evangelista@concrete.com.br>
2017-11-14 16:40:30 -0200

    Pegando Id Clicado para posterior busca no service

--- angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
+++ angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
+import { ContatoService } from './contato.service';
 
 @Component({
     moduleId: module.id,
     selector: 'contato-detalhe',
     templateUrl: 'contato-detalhe.component.html'
 })
-export class ContatoDetalheComponent{
+export class ContatoDetalheComponent implements OnInit {
 
+    constructor(
+        private ContatoService,
+        private route: ActivatedRoute,
+        private location: Location
+    ) { }
+
+    ngOnInit(): void {
+        //Será chamado sempre que ocorrer um clique em algum elemento da lista
+        console.log('clicou em um elemento da lista de contatos');
+        this.route.params.forEach((params: Params) => {
+            //colocando o '+' na frente o retorno do params é convertido para numero
+            let id: number = +params['id'];
+            console.log('Consultando ID: ' + id);
+        });
+    }
 }
