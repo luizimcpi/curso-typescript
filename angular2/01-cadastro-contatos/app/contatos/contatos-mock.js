64163dd383965baffc5ede25c8a486e6c9c366d1
Luiz Henrique <luiz.hse@hotmail.com>
2018-03-14 17:02:05 -0300

    Remove Propertie

--- angular2/01-cadastro-contatos/app/contatos/contatos-mock.js
+++ angular2/01-cadastro-contatos/app/contatos/contatos-mock.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.CONTATOS = [
     { id: 1, nome: 'Luiz Henrique', email: 'luiz.hse@hotmail.com', telefone: '(13) 3273-6667' },
     { id: 2, nome: 'Tania', email: 'tania@uol.com.br', telefone: '(00) 0000-0000' },
