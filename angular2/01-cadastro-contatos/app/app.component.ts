4cc82a4ef52fbca8e6ac42d7ae109f28e0e9a0f4
Luiz Henrique <luiz.hse@hotmail.com>
2018-03-14 22:00:07 -0300

    Remove projeto de cadastro de contatos

--- angular2/01-cadastro-contatos/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-/*O nome do arquivo é sempre em lowercase e o nome da classe é a concatenação do nome do Arquivo sem a extensão*/
-@Component({
-    moduleId: module.id,
-    selector: 'my-app',
-    templateUrl:'app.component.html'
-})
-export class AppComponent{}
